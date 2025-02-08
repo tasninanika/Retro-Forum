c1ae9099513a4d6f8bb7588aefc4eadb69005652
anikatasnin <jarintasnin09@gmail.com>
2025-02-08 23:12:12 +0600

    added tailwind and daisyui

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2 className="text-6xl">hit</h2>
+      <button className="btn">Default</button>
+    </>
+  );
+}
+
+export default App;
